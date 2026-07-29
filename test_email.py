fa93a36cbdf80c10f93a466eff02e081bd5526b1
JeffP-dev <jpecqueur@free.fr>
2026-07-29 10:01:15 +0200

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -4,12 +4,11 @@ from email.mime.text import MIMEText
 
 msg = MIMEText("Ceci est un test d'envoi depuis GitHub Actions.")
 msg["Subject"] = "Test VintedScanner"
-msg["From"] = os.environ.get("SMTP_USERNAME")
+msg["From"] = os.environ.get("SMTP_FROM_ADDRESS")
 msg["To"] = os.environ.get("SMTP_TO")
 
 with smtplib.SMTP(os.environ.get("SMTP_SERVER", "mail.zaclys.net"), 587) as server:
     server.starttls()
     server.login(os.environ.get("SMTP_USERNAME"), os.environ.get("SMTP_PSW"))
-    server.sendmail(msg["From"], [msg["To"]], msg.as_string())
-
+    server.sendmail(os.environ.get("SMTP_FROM_ADDRESS")
 print("Email envoyé avec succès !")
