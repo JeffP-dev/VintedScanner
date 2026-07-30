90a0b9578437887ace1c6bab6f387f575a4bec0a
JeffP-dev <jpecqueur@free.fr>
2026-07-30 20:20:30 +0200

    Update vinted_scanner.py

--- vinted_scanner.py
+++ vinted_scanner.py
@@ -260,6 +260,9 @@ def main(dry_run=False):
 
             item_id = item.get("id")
             item_title = item.get("title")
+            required_keyword = params.get('required_keyword')
+            if required_keyword and required_keyword.lower() not in item_title.lower():
+               continue
             item_url = item.get("url")
             if item_id is None or not item_title or not item_url:
                 logging.warning(
